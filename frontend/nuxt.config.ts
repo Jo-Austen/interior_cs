c11dcb7a1d5e6b7351c3aceb222d9c83c9e4da87
Benjen Zhou <www.bbmac@outlook.com>
2026-01-10 13:06:07 +0800

    feat(deploy): dockerize frontend and complete test environment deployment

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -22,7 +22,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000'
+      // apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000'
+      apiBase: 'http://localhost:8000'
+
 }
 }
 })
